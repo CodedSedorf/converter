6ff0109d8fed1b369fcad50869515888d32f08e4
CodedSedorf <sedorfdlc@gmail.com>
2025-07-11 02:18:31 +0100

    for loop

---
+++ for_loop.py
@@ -0,0 +1,27 @@
+# for item in "Python":
+#   print(item)
+#using for and list
+# for item in ['Sedorf', 'Oladele', 'Saheed']:
+#   print(item)
+#Range
+# for item in range(10):
+#   print(item)
+
+# for num in range(5, 15):
+#   print(num)
+# for item in range(5, 10, 2):
+#   print(item)
+#List is declare using the square bracket. 
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+  total += price
+print(f"Total: {total}")
+
+
+fruits = ["Cherry", "Banana", "Apple", "Orange", "Watermelon"]
+for x in fruits:
+  if x == "Apple":
+    break
+  print(x)
+
